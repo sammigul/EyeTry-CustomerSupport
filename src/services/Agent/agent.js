fb688ee3e47d3e71ff20c2efa4c411cb7d771986
Abdul Sammi Gul <sammigul11005@gmail.com>
2023-11-26 13:05:54 +0500

    api calls for agent profile interactions

---
+++ src/services/Agent/agent.js
@@ -0,0 +1,60 @@
+import { authenticatedAxiosInstance } from '../../api/config';
+
+
+export const viewAgentProfile = async () => {
+    try {
+        const response = await authenticatedAxiosInstance.get(`/agent/profile`);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const changeAgentPassword = async (data) => {
+    try {
+        const response = await authenticatedAxiosInstance.put(`/agent/update_password`, data);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const updateAgentProfile = async (data) => {
+    try {
+        const response = await authenticatedAxiosInstance.put(`/agent/update_profile`, data);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+export const viewAgentPhoto = async () => {
+    try {
+        const response = await authenticatedAxiosInstance.get(`/agent/view_image_server`);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+
+export const updateAgentPhoto = async (formData) => {
+    try {
+        const response = await authenticatedAxiosInstance.post(`/agent/upload_image_server/`, formData, { headers: { 'Content-Type': 'multipart/form-data' } });
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const deleteAgentPhoto = async () => {
+    try {
+        const response = await authenticatedAxiosInstance.delete(`/agent/remove_image_server`);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
