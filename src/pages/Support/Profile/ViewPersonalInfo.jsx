9ad40bb373e8f97cce64d13b3dc8402864dbc8e6
Abdul Sammi Gul <sammigul11005@gmail.com>
2023-11-26 13:09:38 +0500

    fixed styling

--- src/pages/Support/Profile/ViewPersonalInfo.jsx
+++ src/pages/Support/Profile/ViewPersonalInfo.jsx
@@ -10,27 +10,27 @@ const ViewPersonalInfo = () => {
     const [email, setEmail] = React.useState('')
 
     return (
-        <div class="flex items-center justify-center h-screen">
+        <div className="flex items-center justify-center h-screen">
             <div className="bg-white border border-gray-200 rounded-lg shadow  w-[80%]  md:w-[65%] mx-auto mb-10">
                 <div className="flex flex-row mt-5">
                     <div className="left-0 pl-3 flex items-center pointer-events-none">
                         <BiEdit size={30} className="mr-5 pb-1" />
                     </div>
-                    <div class="flex items-center justify-between w-full">
-                        <h2 class="mr-auto text-xl font-bold tracking-tight text-gray-900 dark:text-white">Personal Information</h2>
-                        <Link to='/edit_personal_info'><button class="py-1 px-4 rounded inline-flex items-center ml-auto
+                    <div className="flex items-center justify-between w-full">
+                        <h2 className="mr-auto text-xl font-bold tracking-tight text-gray-900 dark:text-white">Personal Information</h2>
+                        <Link to='/edit_personal_info'><button className="py-1 px-4 rounded inline-flex items-center ml-auto
          bg-transparent hover:bg-blue-500 text-blue-700 font-semibold 
          hover:text-white border border-blue-500 hover:border-transparent justify-end mr-5">
-                            <BiEdit size={20} class="mr-2" />
+                            <BiEdit size={20} className="mr-2" />
                             <span>Edit</span>
                         </button></Link>
                     </div>
                 </div>
-                <hr class="border-3 border-gray-300 my-4" />
+                <hr className="border-3 border-gray-300 my-4" />
                 <div className="p-5">
                     <div className="flex flex-row space-x-4">
                         <div className="flex-grow">
-                            <label for="firstname" className="block text-sm font-semibold text-gray-800 font-sans">First Name</label>
+                            <label htmlFor="firstname" className="block text-sm font-semibold text-gray-800 font-sans">First Name</label>
                             <div className="relative">
                                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                     <FaUser color='grey' />
@@ -41,7 +41,7 @@ const ViewPersonalInfo = () => {
                             </div>
                         </div>
                         <div className="flex-grow">
-                            <label for="lastname" className="block text-sm font-semibold text-gray-800 font-sans">Last Name</label>
+                            <label htmlFor="lastname" className="block text-sm font-semibold text-gray-800 font-sans">Last Name</label>
                             <div className="relative">
                                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                     <FaUser color='grey' />
@@ -54,7 +54,7 @@ const ViewPersonalInfo = () => {
                     </div>
 
 
-                    <label for="email" className="block text-sm mt-5 font-semibold text-gray-800 font-sans">Email</label>
+                    <label htmlFor="email" className="block text-sm mt-5 font-semibold text-gray-800 font-sans">Email</label>
                     <div className="relative">
                         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                             <FaRegEnvelope color='grey' />
