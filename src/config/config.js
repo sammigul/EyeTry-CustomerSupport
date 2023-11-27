883fd2674a862af12f29be2b1198989913dc2970
Abdul Sammi Gul <sammigul11005@gmail.com>
2023-11-27 12:04:05 +0500

    Added exposed URL for API_Server

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
-const API_URL = "http://localhost:3000";
-
+// const API_URL = "http://localhost:3000";
+// Exposed URL for localhost 
+const API_URL = "https://7776-203-124-40-245.ngrok-free.app";
 export default API_URL;
